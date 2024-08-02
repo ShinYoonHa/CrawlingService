cb6708595a6cdc43916506d361e6c130b868b008
ShinYoonHa <idc06012@naver.com>
2024-08-02 16:06:15 +0900

    First Commit

---
+++ src/test/java/com/crawl/Crawling/CrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crawl.Crawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
