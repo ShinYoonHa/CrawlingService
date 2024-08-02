133dcbce7c8f785084c9d47fb8e2f40bb2555d38
ShinYoonHa <idc06012@naver.com>
2024-08-02 16:08:41 +0900

    feat : PriceHistory, Product 레포지토리 생성

---
+++ src/main/java/com/crawl/Crawling/repository/PriceHistoryRepository.java
@@ -0,0 +1,12 @@
+package com.crawl.Crawling.repository;
+
+import com.crawl.Crawling.entity.PriceHistory;
+import com.crawl.Crawling.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PriceHistoryRepository extends JpaRepository<PriceHistory, Long> {
+    //상품 정보로 상품에 대한 가격이력 리스트 반환
+    List<PriceHistory> findByProduct(Product product);
+}
