b667c43aab5cea2f3ba5802a6aed1a51b21be893
ShinYoonHa <idc06012@naver.com>
2024-08-02 20:32:07 +0900

    refactor : 상품 Entity에 카테고리 속성 추가

--- src/main/java/com/crawl/Crawling/entity/Product.java
+++ src/main/java/com/crawl/Crawling/entity/Product.java
@@ -1,5 +1,6 @@
 package com.crawl.Crawling.entity;
 
+import com.crawl.Crawling.constant.Category;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,6 +19,9 @@ public class Product {
     private String img; //상품 이미지 경로
     private Double rate; //평점
     private int rate_count; //평점 개수
+
+    @Enumerated(EnumType.STRING)
+    private Category category; //카테고리
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<PriceHistory> priceHistories;
