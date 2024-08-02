643350c1e342eab920d72aabb50aeaa59250701e
ShinYoonHa <idc06012@naver.com>
2024-08-02 20:30:47 +0900

    refactor : 크롤링된 상품 상세 데이터 크롤링 추가(Testing)

--- src/main/java/com/crawl/Crawling/controller/CrawlingController.java
+++ src/main/java/com/crawl/Crawling/controller/CrawlingController.java
@@ -1,16 +1,40 @@
 package com.crawl.Crawling.controller;
 
 import com.crawl.Crawling.service.CrawlingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
 
+@RequestMapping(value = "/crawl")
 @Controller
 public class CrawlingController {
-    @GetMapping(value = "/crawl")
-    public String crawl() throws IOException {
-        CrawlingService crawlingService = new CrawlingService();
-        return "crawl";
+    @Autowired
+    CrawlingService crawlingService;
+
+    @GetMapping(value = "")
+    public String crawl() {
+        try {
+            crawlingService.crawl();
+            return "crawl";
+
+        } catch(IOException e) {
+            e.printStackTrace();
+            return "크롤링 중 오류가 발생했습니다";
+        }
+    }
+
+    @GetMapping(value = "/detailCrawl")
+    public String detailCrawl() {
+        try {
+            crawlingService.detailCrawl(8052075526L);
+            return "detailCrawl";
+
+        } catch(IOException e) {
+            e.printStackTrace();
+            return "크롤링 중 오류가 발생했습니다";
+        }
     }
 }
