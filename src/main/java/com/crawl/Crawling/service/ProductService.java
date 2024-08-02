7a0494e9c7915dfd4d03641ea68469c190829d29
ShinYoonHa <idc06012@naver.com>
2024-08-02 20:32:42 +0900

    refactor : 상품 크롤링 시 중복데이터 확인 및 제품 가격 이력 로직 추가

--- src/main/java/com/crawl/Crawling/service/ProductService.java
+++ src/main/java/com/crawl/Crawling/service/ProductService.java
@@ -1,14 +1,13 @@
 package com.crawl.Crawling.service;
 
-import com.crawl.Crawling.entity.PriceHistory;
 import com.crawl.Crawling.entity.Product;
 import com.crawl.Crawling.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -23,8 +22,36 @@ public class ProductService {
         return productRepository.save(product);
     }
 
+    public void saveAllProducts(List<Product> products) {
+        for(Product product : products) {
+            //중복 데이터 확인 및 처리
+            Product existingProduct = productRepository.findById(product.getId()).orElse(null);
+            if(existingProduct != null) {
+                //기존 제품이 있을경우, 기존 제품에 대해 변경된 필드만 업데이트
+                existingProduct.setName(product.getName());
+                existingProduct.setPrice(product.getPrice());
+                existingProduct.setImg(product.getImg());
+                existingProduct.setRate(product.getRate());
+                existingProduct.setRate_count(product.getRate_count());
+                productRepository.save(existingProduct);
+
+                //가격 이력 저장
+                priceHistoryService.savePriceHistory(existingProduct, product.getPrice());
+            } else {
+                //기존 제품이 없을 경우 새 제품 추가
+                productRepository.save(product);
+                //새 제품에 대한 가격 이력 추가
+                priceHistoryService.savePriceHistory(product, product.getPrice());
+            }
+        }
+    }
+
+    //모든 상품 조회
     public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+    public Optional<Product> getProduct(Long item_id) {
+        return productRepository.findById(item_id);
     }
 
     //상품가격 업데이트 및 새로운 가격 이력 저장
@@ -33,12 +60,7 @@ public class ProductService {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new RuntimeException("상품이 존재하지 않습니다."));
 
-        // 새로운 가격 정보를 History에 추가
-        PriceHistory priceHistory = new PriceHistory();
-        priceHistory.setProduct(product);
-        priceHistory.setPrice(newPrice);
-        priceHistory.setDate(LocalDate.now());
-        priceHistoryService.savePriceHistory(priceHistory); // PriceHistoryService 사용
+        priceHistoryService.savePriceHistory(product, newPrice); // PriceHistoryService 사용
 
         // 현재 가격 정보 수정
         product.setPrice(newPrice);
