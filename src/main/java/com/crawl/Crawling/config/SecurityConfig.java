542edc352c576ecf986c6da42aa08ae7284a222a
ShinYoonHa <idc06012@naver.com>
2024-08-19 21:23:20 +0900

    refactor : 회원가입을 위한 Security 설정

--- src/main/java/com/crawl/Crawling/config/SecurityConfig.java
+++ src/main/java/com/crawl/Crawling/config/SecurityConfig.java
@@ -1,24 +1,46 @@
 package com.crawl.Crawling.config;
 
+import com.crawl.Crawling.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    UserService userService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/css/**", "/js/**", "/img/**", "/favicon.ico", "/error").permitAll()
-                .requestMatchers("/", "/images/**", "/crawl/**", "/**").permitAll()
+                .requestMatchers("/", "/images/**", "/crawl/**", "/user/**", "/**").permitAll()
                 .anyRequest().authenticated()
-        );
+        ).formLogin(formLogin -> formLogin
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/") //성공하면 / 로 감
+                .usernameParameter("email") //email 가지고 memberService로 감
+                .failureUrl("/user/login/error") //실패 시 여기로 감
+        )
+        ;
         // CORS 설정 추가
         http.cors(withDefaults());
 
         return http.build();
+    }
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 }
