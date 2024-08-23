7c8e26afe5f2fbc331e1a6e60c1f4dc4b638e4a2
ShinYoonHa <idc06012@naver.com>
2024-08-23 12:23:19 +0900

    feat : 좋아요한 상품 목록에 선택삭제, 전체선택 기능 추가

--- src/main/java/com/crawl/Crawling/repository/LikesRepository.java
+++ src/main/java/com/crawl/Crawling/repository/LikesRepository.java
@@ -5,6 +5,10 @@ import com.crawl.Crawling.entity.Product;
 import com.crawl.Crawling.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LikesRepository extends JpaRepository<Likes, Long> {
     Likes findByUserAndProduct(User user, Product product);
+    void deleteByUserAndProduct(User user, Product product);
+    List<Likes> findByUser(User user);
 }
--- src/main/java/com/crawl/Crawling/service/LikesService.java
+++ src/main/java/com/crawl/Crawling/service/LikesService.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -16,9 +18,9 @@ public class LikesService {
 
     public void toggleLikes(User user, Product product) { //product에 좋아요 누름/취소 toggle 메소드
         Likes likes = findByUserAndProduct(user, product);
-        if(likes != null) { //user가 product에 좋아요 눌러서 db에 likes 값이 있을 경우
+        if(likes != null) { //사용자가 상품을 좋아요한 이력이 db에 있을 경우
             likesRepository.delete(likes); //삭제
-        } else { //db에 해당 product에 대한 user의 likes가 없을 경우
+        } else { //사용자가 상품을 좋아요 한적이 없을 경우
             Likes newLikes = new Likes();
             newLikes.setUser(user);
             newLikes.setProduct(product);
@@ -26,6 +28,7 @@ public class LikesService {
             likesRepository.save(newLikes);
         }
     }
+    //User와 Product로 좋아요 이력 찾기 (단일 반환값)
     public Likes findByUserAndProduct(User user, Product product) {
         try {
             Likes likes = likesRepository.findByUserAndProduct(user, product);
@@ -33,6 +36,14 @@ public class LikesService {
         } catch (NullPointerException e) {
             return null;
         }
+    }
+    //User로 좋아요 이력 찾기 (다중 반환값)
+    public List<Likes> findByUser(User user) {
+        return likesRepository.findByUser(user);
+    }
+
+    public void removeLike(User user, Product product) {
+        likesRepository.deleteByUserAndProduct(user, product);
     }
 
 }
